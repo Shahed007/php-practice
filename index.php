0bd8adcc1f033d12983ec96e6e3d8f465f3f2797
Shahed <sk0178397@gmial.com>
2024-06-07 23:59:16 +0600

    Writed more php code.

--- index.php
+++ index.php
@@ -7,18 +7,28 @@
 </head>
 <body>
   <?php 
-    echo "Hello World"
-  ?>
-  <!-- <?php 
-   phpinfo();
-  ?> -->
-  <?php 
-  function get_var($varname){
-    global $$varname;
-    var_dump($$varname);
-  }
+  //  $txt = "PHP";
+  //  echo "I love $txt!";
+  //  echo phpversion();
+  //  ECHO "Hello World<br/>";
+  //  echo "Hello World<br/>";
+  //  Echo "Hello World<br/>";
+   // This is single line comment
+   # This is also single line comment
+   /* This is a multiline comment */
+ 
+   $text = "W3Schools.com";
+   echo "I love". $text ."!";
 
-   get_var('_POST');
+   $x = 5;
+   $y = $x + 4;
+   echo $y;
+   
+   echo "<h2>PHP is Fun!</h2> <br/>";
+   echo "Hello World! <br/>";
+   echo "I'am about learn PHP!<br/>";
+   echo "This", "String", "was", "made", "with multiple parameters.";
   ?>
+ 
 </body>
 </html>
